7d714ed2aff7e75999db9c62b20a045e8d184ff3
Seth Raphael <git@magicseth.com>
2026-02-08 02:48:01 -0800

    fix auth

--- camps/app/callback/route.ts
+++ camps/app/callback/route.ts
@@ -1,3 +1,17 @@
 import { handleAuth } from '@workos-inc/authkit-nextjs';
+import { NextRequest } from 'next/server';
 
-export const GET = handleAuth();
+function replaceRedirectUriBasedOnHostname(request: NextRequest) {
+  const hostname = request.headers.get('host');
+  if (hostname === 'localhost:3000') {
+    return 'http://localhost:3000/callback';
+  }
+  return `https://${hostname}/callback`;
+}
+function handleAuthWithAfterPatchingProcessEnv(request: NextRequest) {
+  process.env.NEXT_PUBLIC_WORKOS_REDIRECT_URI = replaceRedirectUriBasedOnHostname(request);
+  process.env.WORKOS_REDIRECT_URI = replaceRedirectUriBasedOnHostname(request);
+  return handleAuth();
+}
+
+export const GET = handleAuthWithAfterPatchingProcessEnv;
